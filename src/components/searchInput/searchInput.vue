3825841db88461c7be250a12519621e65c986c99
Mrezagolbaba <m.reza.golbaba@gmail.com>
2022-06-28 07:23:52 +0430

    add serach input component

---
+++ src/components/searchInput/searchInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form class="form-inline mt-1">
+      <input
+        type="search"
+        v-model="searchtext"
+        placeholder="Search"
+        class="form-control mr-sm-2 text-center"
+      />
+      <div v-if="searchtext">
+        <router-link
+          :to="{ name: 'search-details', params: { searchText: searchtext } }"
+        >
+          <button type="submit" class="btn btn-primary my-2 my-sm-0">
+            Search
+          </button>
+        </router-link>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      searchtext: "",
+    };
+  },
+};
+</script>
